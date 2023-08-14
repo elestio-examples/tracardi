dd7634c14ed6e3bb1618ec32a8928e17a307cdb3
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-14 18:21:54 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,3 +6,14 @@ echo "Waiting for software to be ready ..."
 sleep 30s;
 
 
+target=$(docker-compose port tracardi-staging-api 80)
+
+curl http://${target}/install \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'authorization: Bearer None' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,8 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+mkdir -p ./es_data
+chown -R 1000:1000 ./es_data
+
+mkdir -p ./data
+chown -R 1000:1000 ./data
