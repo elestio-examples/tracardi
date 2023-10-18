ae5aa70627750b7e09031d060bcf4364d7f5382f
Prem chavhan <pchavhan@logiquad.com>
2023-10-18 19:25:15 +0530

    fix

--- tests/api/build.sh
+++ tests/api/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -f docker-compose.yaml
 mv docker-compose-new.yml docker-compose.yml
-mv docker.Dockerfile Dockerfile
+mv docker.ssl.Dockerfile Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/tracardi-api:latest | docker load
