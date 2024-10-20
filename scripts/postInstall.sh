5774d407acd6f7b9014b9f8082f165a0ba446f48
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-10-20 16:12:36 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -8,12 +8,14 @@ sleep 30s;
 
 target=$(docker-compose port tracardi-staging-api 80)
 
-curl http://${target}/install \
+  curl http://${target}/install \
   -H 'accept: application/json, text/plain, */*' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'authorization: Bearer None' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -H 'pragma: no-cache' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' \
-  --data-raw '{"username":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"}' \
-  --compressed
+  -H 'priority: u=1, i' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36' \
+  -H 'x-client: tracardi-gui' \
+  -H 'x-context: staging' \
+  --data-raw '{"username":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","token":"tracardi","needs_admin":true,"update_mapping":false}'
